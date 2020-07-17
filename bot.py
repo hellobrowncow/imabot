5b4abd2d4837aa0818d37947086c733c534faa00
lamvwl <lamvwl@users.noreply.github.com>
2020-07-17 14:40:45 -0400

    Added found text and response loop

--- bot.py
+++ bot.py
@@ -10,3 +10,15 @@ ACCESS_SECRET = ''
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+
+def respond_to_mentioned_text():
+    mentions = api.mentions.timeline()
+    for mention in mentions
+      print(mention.id + '-' + mention.text)
+      if 'FOUNDTEXT' in mention.text.lower():
+        print('found FOUNDTEXT')
+        print('response ')
+
+while True:
+    respond_to_mentioned_text()
+    time.sleep(300)
