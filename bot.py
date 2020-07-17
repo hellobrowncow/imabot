302485e352e6425bec1314f92bd2f455dc69c050
lamvwl <lamvwl@users.noreply.github.com>
2020-07-17 13:34:06 -0400

    Keys

--- bot.py
+++ bot.py
@@ -1,3 +1,12 @@
 import tweepy
 
 print('imabot')
+
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
